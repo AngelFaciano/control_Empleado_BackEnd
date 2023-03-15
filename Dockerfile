c7201e3cd66850a8deaccf67752ebe2bba99c8da
AngelFaciano <126618958+AngelFaciano@users.noreply.github.com>
2023-03-14 22:06:06 -0300

    agregar cors

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM amazoncorretto:17-alpine-jdk
-maintainer FAF
-COPY target/gestion.empleado-0.0.1-SNAPSHOT.jar faf-app.jar
-ENTRYPOINT ["java","-jar","/faf-app.jar"]
--- src/main/java/com/gestion/empleado/Controller/EmpleadoController.java
+++ src/main/java/com/gestion/empleado/Controller/EmpleadoController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = {"http://localhost:4200/","https://portfoliofacianofa.web.app/","https://portfoliofacianofa.web.app"})
 public class EmpleadoController {
     @Autowired
     IEmpleado iEmpleado;
